7972da26dd96670b9d03e6a3ba11843f4c3e7f37
AtticusPham <thaipham7778@gmail.com>
2020-07-03 18:55:04 +0700

    edited

--- Node.js/demo_server/server.js
+++ Node.js/demo_server/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const publicPath = path.join(__dirname, 'public');
 
-app.use('/public', express.static(publicPath));
+
+
+app.use(express.static("public"));
+
 
 app.get('/', function(req, res) {
     var dir = path.join(__dirname, 'public/home.html');
