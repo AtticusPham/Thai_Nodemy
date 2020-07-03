cb9131ae8a5abc629637bfd281a50f8547807a44
AtticusPham <62809733+AtticusPham@users.noreply.github.com>
2020-07-03 21:44:20 +0700

    Update server.js

--- Node.js/demo_server/server.js
+++ Node.js/demo_server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 app.use(express.static("public"));
 
-
+const port = 3000;
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/home.html'));
-}).listen(3000)
+}).listen(port, ()=> console.log(`app listens at http://localhost:${port}`));
